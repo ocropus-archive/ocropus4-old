9755bfccc5b471a4a4f03b1ec17dffda2c9192ac
Tom <tmbdev+github@gmail.com>
2020-12-21 06:09:52 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -29,12 +29,12 @@ scikit-image
 scipy
 humanhash3
 tabulate
--e git+git://github.com/tmbdev/webdataset.git#egg=webdataset
--e git+git://github.com/NVlabs/ocrodeg.git#egg=ocrodeg
--e git+git://github.com/NVlabs/tensorcom.git#egg=tensorcom
--e git+git://github.com/tmbdev/tarproc.git#egg=tarproc
--e git+git://github.com/tmbdev/torchmore.git#egg=torchmore
--e git+git://github.com/NVlabs/ocrodeg.git#egg=ocrodeg
+git+git://github.com/tmbdev/webdataset.git#egg=webdataset
+git+git://github.com/NVlabs/ocrodeg.git#egg=ocrodeg
+git+git://github.com/NVlabs/tensorcom.git#egg=tensorcom
+git+git://github.com/tmbdev/tarproc.git#egg=tarproc
+git+git://github.com/tmbdev/torchmore.git#egg=torchmore
+git+git://github.com/NVlabs/ocrodeg.git#egg=ocrodeg
 """.split()
 print(PREREQS)
 
