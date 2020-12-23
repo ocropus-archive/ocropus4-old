ada19587ac4b5261e7ddb056c2c391cb3960f30b
Tom <tmbdev+github@gmail.com>
2020-12-23 12:22:36 -0800

    added models

---
+++ models/segunet.py
@@ -0,0 +1,14 @@
+from torch import nn
+from torchmore import flex, layers, combos
+
+
+def make_model(noutput=4):
+    model = nn.Sequential(
+        layers.Input("BDHW", range=(0, 1), sizes=[None, 1, None, None]),
+        *combos.conv2d_block(64, 3, repeat=2),
+        combos.make_unet([96, 128, 192, 256]),
+        *combos.conv2d_block(64, 3, repeat=2),
+        flex.Conv2d(noutput, 3, padding=1),
+    )
+    flex.shape_inference(model, (1, 1, 256, 256))
+    return model
