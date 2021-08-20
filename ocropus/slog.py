c81cb360b65f88895f24bdbb6c4b474137319e11
Thomas Breuel <tbreuel@nvidia.com>
2021-08-19 20:31:39 -0700

    changed augmentation in ocrorec, additional slog commands

--- ocropus/slog.py
+++ ocropus/slog.py
@@ -273,6 +273,7 @@ class Logger:
 @app.command()
 def getargs(fname):
     """Get the arguments used for training from a logfile."""
+    assert os.path.exists(fname), fname
     con = sqlite3.connect(fname)
     query = "select json from log where key='args'"
     result = list(con.execute(query))
@@ -285,6 +286,7 @@ def getargs(fname):
 @app.command()
 def getmodel(fname):
     """Get the model definition used for training from a logfile."""
+    assert os.path.exists(fname), fname
     con = sqlite3.connect(fname)
     query = "select obj from log where key='model'"
     result = list(con.execute(query))
@@ -299,6 +301,7 @@ def getstep(fname, step: int, output=None):
     """Get the model at the given step."""
     assert output is not None
     assert not os.path.exists(output)
+    assert os.path.exists(fname), fname
     con = sqlite3.connect(fname)
     query = f"select obj, step, scalar from log where key='model' order by abs({step}-step) limit 1"
     result = list(con.execute(query))
@@ -310,6 +313,7 @@ def getstep(fname, step: int, output=None):
 
 
 def gettrained(fname, query, output=None):
+    assert os.path.exists(fname), fname
     con = sqlite3.connect(fname)
     result = list(con.execute(query))
     assert len(result) > 0, "found no model"
@@ -343,6 +347,7 @@ def getlast(fname, output=None):
 @app.command()
 def val2model(fname):
     """Move the validation values into the scalar field for the corresponding model."""
+    assert os.path.exists(fname), fname
     con = sqlite3.connect(fname)
     print(list(con.execute("select step, scalar from log where key='model'")))
     query = "select step, scalar from log where key='val/err'"
@@ -358,6 +363,7 @@ def val2model(fname):
 
 @app.command()
 def schema(fname):
+    assert os.path.exists(fname), fname
     con = sqlite3.connect(fname)
     print(list(con.execute("select sql from sqlite_master where name = 'log'"))[0][0])
     print(list(con.execute("select distinct(key) from log")))
@@ -366,6 +372,7 @@ def schema(fname):
 @app.command()
 def show(fname, keys="model", xscale="linear", yscale="linear"):
     plt.title(fname)
+    assert os.path.exists(fname), fname
     con = sqlite3.connect(fname)
     plt.xscale(xscale)
     plt.yscale(yscale)
@@ -395,6 +402,9 @@ def info(fnames: List[str], verbose: bool = False):
     result = []
     for fname in fnames:
         try:
+            if not os.path.exists(fname):
+                print(f"{fname}: not found", file=sys.stderr)
+                continue
             con = sqlite3.connect(fname)
             args = list(con.execute("select json from log where key='args'"))
             if len(args) == 0:
@@ -444,6 +454,9 @@ def findempty(fnames: List[str], n=0):
     fnames = sorted(fnames)
     for fname in fnames:
         try:
+            if not os.path.exists(fname):
+                print(f"{fname}: not found", file=sys.stderr)
+                continue
             con = sqlite3.connect(fname)
             models = list(
                 con.execute(
@@ -465,6 +478,9 @@ def steps(fnames: List[str], clean:int=-1):
     fnames = sorted(fnames)
     for fname in fnames:
         try:
+            if not os.path.exists(fname):
+                print(f"{fname}: not found", file=sys.stderr)
+                continue
             con = sqlite3.connect(fname)
             result = list( con.execute("select max(step) from log"))
             value = result[0][0]
