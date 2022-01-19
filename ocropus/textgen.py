2e0881fe99c1002ac743d3d22b90fff822e15565
Tom <tmbdev+github@gmail.com>
2022-01-19 02:19:48 -0800

    updated default textgen all parameters

--- ocropus/textgen.py
+++ ocropus/textgen.py
@@ -226,16 +226,16 @@ def generate_(*args, **kw):
 
 @app.command()
 def all():
-    nw = 100
-    nshards = 1
+    nw = 50000
+    nshards = 100
     ray.init()
     ray.get([generate_.remote(output=f"_core-words-{i:06d}.tar", nwords=nw, fontlist="core", generator="words") for i in range(nshards)])
     ray.get([generate_.remote(output=f"_core-text-{i:06d}.tar", nwords=nw, fontlist="core") for i in range(nshards)])
+    ray.get([generate_.remote(output=f"_core-numbers-{i:06d}.tar", nwords=nw, fontlist="core", generator="numbers") for i in range(nshards)])
+    ray.get([generate_.remote(output=f"_core-ascii-{i:06d}.tar", nwords=nw, fontlist="core", generator="ascii") for i in range(nshards)])
+    ray.get([generate_.remote(output=f"_google-numbers-{i:06d}.tar", nwords=nw, fontlist="google", generator="numbers") for i in range(nshards)])
     ray.get([generate_.remote(output=f"_google-text-{i:06d}.tar", nwords=nw, fontlist="google") for i in range(nshards)])
     ray.get([generate_.remote(output=f"_italics-text-{i:06d}.tar", nwords=nw, fontlist="italics") for i in range(nshards)])
-    ray.get([generate_.remote(output=f"_core-numbers-{i:06d}.tar", nwords=nw, fontlist="core", generator="numbers") for i in range(nshards)])
-    ray.get([generate_.remote(output=f"_google-numbers-{i:06d}.tar", nwords=nw, fontlist="google", generator="numbers") for i in range(nshards)])
-    ray.get([generate_.remote(output=f"_core-ascii-{i:06d}.tar", nwords=nw, fontlist="core", generator="ascii") for i in range(nshards)])
 
 if __name__ == "__main__":
     app()
